d700598d6aa30f214ca37beb2819e8ea003b5375
jferdizzle <julianferdman@gmail.com>
2024-08-20 20:53:48 -0400

    hello old mistcoin

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory, redirect, url_for
+
+app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def catch_all(path):
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
